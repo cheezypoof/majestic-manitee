b46f3cf7ed3ea8738680433e75ab4cc77142ed93
Haulson Wong <shirom@gmail.com>
2013-02-24 15:45:23 -0800

    boost block redone

--- New Unity Project 1/Assets/Scripts/BoostBlock.cs
+++ New Unity Project 1/Assets/Scripts/BoostBlock.cs
@@ -18,9 +18,9 @@ public class BoostBlock : SpecialLevelBlock {
     {
         Debug.Log("BoostBlock");
 
-        //todo make force based on rotation of game object
-        //Quaternion rot=gameObject.transform.rotation;
-        Vector3 force=Vector3.up*boostForce;
+        //force based on rotation of game object
+        
+        Vector3 force=transform.up*boostForce;
         other.rigidbody.velocity = new Vector3(0,0,0);
         other.rigidbody.AddForce(force, ForceMode.Impulse);
     }
