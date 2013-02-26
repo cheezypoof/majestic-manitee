5f7e16bcca427f9dabc790929ed09ee81ce4c960
alan voong <alanvoongis@gmail.com>
2013-02-25 22:01:20 -0800

    sky now follows player

--- New Unity Project 1/Assets/Scripts/bgLogic.cs
+++ New Unity Project 1/Assets/Scripts/bgLogic.cs
@@ -5,16 +5,14 @@ public class bgLogic : MonoBehaviour {
     private float xLoc;
     private float yLoc;
     public GameObject sky;
+    public GameObject clouds;
 
 	// Use this for initialization
 	void Start () {
-        sky.transform.position.Set(this.transform.position.x, this.transform.position.y, 20);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        xLoc = transform.position.x;
-        yLoc = transform.position.y;
-        sky.transform.Translate(xLoc, yLoc, 20);
+        sky.transform.position = new Vector3(transform.position.x, sky.transform.position.y, sky.transform.position.z);
 	}
 }
